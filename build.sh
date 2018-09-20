f280b5774f0210d6c206eb2c587e05b87f919536
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2018-09-21 01:02:30 +0900

    build.sh作成

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -Ceu
+cd `dirname $0`
+
+TARGET_DIR="./koshian_image_save_button"
+OUTPUT_DIR=".."
+
+script_dir=$(cd $(dirname $0); pwd)
+echo "script_dir = $script_dir"
+addon_name=${script_dir##*/}
+echo "addon_name = $addon_name"
+cd ${TARGET_DIR}
+addon_ver=$(jq -r '.version' manifest.json)
+echo "addon_ver = $addon_ver"
+filename="${OUTPUT_DIR}/${addon_name}-${addon_ver}.zip"
+echo "filename = $filename"
+
+zip -r -9 ${filename} * -x "*.bak" ".eslint*"
